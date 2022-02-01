8a92fc6f62183b7786bdfd143de5efdcfbc7140c
AndreyD <andreydv@bk.ru>
2022-02-01 15:49:13 +0300

    task_2_1

--- Danshin_Andrey_dz_2/task_2_1.py
+++ Danshin_Andrey_dz_2/task_2_1.py
@@ -1,1 +1,31 @@
-Print ('Hello World')
+#Задание 2a
+def convert_list(list_in):
+#фильтруем кривым способом элементы "5" и "17"
+    for i in my_list:
+        if i.isdigit():
+            if len(i) < 2:
+                x = my_list.index(i)
+                my_list[x] = f"', '0{i}', '"
+
+    for i in my_list:
+        if i.isdigit():
+            x = my_list.index(i)
+            my_list[x] = f"', '{i}', '"
+
+#фильтруем еще более кривым способом элементы "+5"
+    for i in my_list:
+        if i.count('+'):
+            if len(i) < 3:
+                x = my_list.index(i)
+                my_list[x] = f"', '+0{i[1:2]}', '"
+    return my_list
+
+
+my_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+result = convert_list(my_list)
+print(result)
+print('end')
+
+#Задание 2b
+my_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+print(' '.join(my_list))
