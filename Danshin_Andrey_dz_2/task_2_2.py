74c89334f770f6f46f840cdccfc9eb8e0b027694
AndreyD <andreydv@bk.ru>
2022-02-01 16:10:50 +0300

    task_2_2

--- Danshin_Andrey_dz_2/task_2_2.py
+++ Danshin_Andrey_dz_2/task_2_2.py
@@ -6,12 +6,10 @@ def convert_list(list_in):
             if len(i) < 2:
                 x = my_list.index(i)
                 my_list[x] = f"', '0{i}', '"
-
     for i in my_list:
         if i.isdigit():
             x = my_list.index(i)
             my_list[x] = f"', '{i}', '"
-
 #фильтруем еще более кривым способом элемент "+5"
     for i in my_list:
         if i.count('+'):
@@ -19,7 +17,6 @@ def convert_list(list_in):
                 x = my_list.index(i)
                 my_list[x] = f"', '+0{i[1:2]}', '"
     return my_list
-
 
 my_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 result = convert_list(my_list)
@@ -35,12 +32,10 @@ def convert_list(list_in):
             if len(i) < 2:
                 x = my_list.index(i)
                 my_list[x] = f" '0{i}' "
-
     for i in my_list:
         if i.isdigit():
             x = my_list.index(i)
             my_list[x] = f" '{i}' "
-
 #фильтруем еще более кривым способом элемент "+5"
     for i in my_list:
         if i.count('+'):
