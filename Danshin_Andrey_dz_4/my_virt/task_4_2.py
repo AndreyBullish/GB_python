d5db9fcc746bcde63528450c547aa24673be2ed7
AndreyD <andreydv@bk.ru>
2022-02-11 16:54:50 +0300

    task_4

--- Danshin_Andrey_dz_4/my_virt/task_4_2.py
+++ Danshin_Andrey_dz_4/my_virt/task_4_2.py
@@ -7,20 +7,33 @@ def currency_rates(code: str) -> float:
     code = code.upper()
     response = requests.get(url)
     cur = response.text
+    start_index = cur.find(code)
+
     if cur.find(code) == -1:
         return None
     else:
-        start_index = cur.find(code)
-        first_cut = cur[start_index:cur.find('</Value', start_index)]
-        second_cut = first_cut.find('<Value') + len('<Value>')
-        result_value = first_cut[second_cut:]
-        result_value = float(result_value.replace(',', '.'))
-        result_value = float('{0:.2f}'.format(result_value))
-        return result_value
+        nominal_first_cut = cur[start_index:cur.find('</Nominal>', start_index)]
+        nominal_second_cut = nominal_first_cut.find('<Nominal>') + len('<Nominal>')
+        nominal_value = int(nominal_first_cut[nominal_second_cut:])
+        if nominal_value == 1:
+            first_cut = cur[start_index:cur.find('</Value', start_index)]
+            second_cut = first_cut.find('<Value') + len('<Value>')
+            result_value = first_cut[second_cut:]
+            result_value = float(result_value.replace(',', '.'))
+            result_value = float('{0:.2f}'.format(result_value))
+            return result_value
+        else:
+            first_cut = cur[start_index:cur.find('</Value', start_index)]
+            second_cut = first_cut.find('<Value') + len('<Value>')
+            result_value = first_cut[second_cut:]
+            result_value = float(result_value.replace(',', '.'))
+            result_value = (float('{0:.2f}'.format(result_value)))/nominal_value
+            return result_value
 
 print(currency_rates("USD"))
-print(currency_rates("eur"))
-print(currency_rates("noname"))
+print(currency_rates("AMD"))
+print(currency_rates("None"))
+
 
 print('\n','end')
 
