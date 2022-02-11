6954cbc5f732cdd91af82120cfcedc3e4fcf33e4
AndreyD <andreydv@bk.ru>
2022-02-11 10:38:42 +0300

    task_4

--- Danshin_Andrey_dz_4/my_virt/task_4_4.py
+++ Danshin_Andrey_dz_4/my_virt/task_4_4.py
@@ -1,3 +1,12 @@
 import utils
 
-print(utils.currency_rates("USD"))
+print(utils.currency_rates("eur"))
+
+'''
+C:\Users\andre\PycharmProjects\pythonProject\GB_python\Danshin_Andrey_dz_4\my_virt>python utils.py
+74.72
+85.38
+None
+
+ end
+'''
--- Danshin_Andrey_dz_4/my_virt/utils.py
+++ Danshin_Andrey_dz_4/my_virt/utils.py
@@ -17,4 +17,9 @@ def currency_rates(code: str) -> float:
         result_value = float('{0:.2f}'.format(result_value))
         return result_value
 
+if __name__ == '__main__':
+    print(currency_rates("USD"))
+    print(currency_rates("eur"))
+    print(currency_rates("noname"))
 
+    print('\n', 'end')
