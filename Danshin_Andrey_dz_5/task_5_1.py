b46aa60989b7d4b2719b47af60b74931721dfecb
AndreyD <andreydv@bk.ru>
2022-02-14 12:16:31 +0300

    task_4

---
+++ Danshin_Andrey_dz_5/task_5_1.py
@@ -0,0 +1,23 @@
+import random
+
+
+def odd_nums(number: int) -> int:
+    """Генератор, возвращающий по очереди нечетные целые числа от 1 до number (включительно)"""
+    for num in range(1, number + 1, 2):
+        yield num
+
+'''
+odd_to_5 = odd_nums(5)
+print(next(odd_to_5)) #1
+print(next(odd_to_5)) #3
+print(next(odd_to_5)) #5
+print(next(odd_to_5)) #StopIteration
+'''
+
+n = 15
+generator = odd_nums(n)
+for _ in range(1, n + 1, 2):
+    print(next(generator))
+#next(generator)  # если раскомментировать, то должно падать в traceback по StopIteration
+
+print('\n' 'end')
