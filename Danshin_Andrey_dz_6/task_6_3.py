149f53e70a6a1b0f914f8a2ac425503560e03a80
AndreyD <andreydv@bk.ru>
2022-02-17 22:47:08 +0300

    task_6

--- Danshin_Andrey_dz_6/task_6_3.py
+++ Danshin_Andrey_dz_6/task_6_3.py
@@ -1,48 +1,48 @@
-import random
 import sys
 import json
-import string
+import random
 
-random_number = random.randrange(5, 10)
-random_number2 = random.randrange(1, 5)
+
+names = ['Александр', 'Андрей', 'Сергей', 'Михаил', 'Антон']
+second_names = ['Иванов', 'Смирнов', 'Кузнецов', 'Попов', 'Васильев']
+middle_names = ['Александрович', 'Андреевич', 'Сергеевич', 'Махайлович', 'Антонович']
+hobbies = ['бег', 'плавание', 'подводное плавание', 'велоспорт', 'танцы', 'чтение', 'пение', 'музыка']
 
 #генератор случайных ФИО
-def generate_random_id(length):
-    letters_upper = string.ascii_uppercase
-    letters_lower = string.ascii_lowercase
-    rand_string = ''.join(random.choice(letters_upper) for i in range(1)) + ''.join(random.choice(letters_lower) for i in range(length))
-    return f'{rand_string},{rand_string},{rand_string}'
+def generate_random_id():
+    random_name = random.choice(names)
+    random_second_names = random.choice(second_names)
+    random_middle_names = random.choice(middle_names)
+    return (f'{random_second_names},{random_name},{random_middle_names}')
 
-#генератор случайных хобби
-def generate_random_hobby(length):
-     letters_lower = string.ascii_lowercase
-     rand_string = ''.join(random.choice(letters_lower) for i in range(length))
-     random_hobby_list = (f'{rand_string},' for i in range(random_number2))
-     random_hobby_str = ''.join(random_hobby_list)
-     return random_hobby_str
+# #генератор случайных хобби
+def generate_random_hobby():
+    random_hobby = random.choice(hobbies)
+    return random_hobby
 
 #создаем документы со именами и хобби
 with open('users.csv', 'w', encoding='utf-8') as fw:
-    for i in range(6):
-        fw.write(f'{generate_random_id(5)}\n')
-with open('hobby.csv', 'w', encoding='utf-8') as fw:
-    for i in range(5):
-        fw.write(f'{generate_random_hobby(10)}\n')
+     for i in range(15):
+         fw.write(f'{generate_random_id()}\n')
 
-#собираем словарь, проверяя разницу между длинами user  hobby
+with open('hobby.csv', 'w', encoding='utf-8') as fw:
+     for i in range(12):
+         fw.write(f'{generate_random_hobby()}, {generate_random_hobby()}\n')
+
+#собираем словарь, проверяя разницу между длинами user hobby
 list_users = []
 list_hobby = []
 
-with open('users.csv', 'r', encoding='utf-8') as f1r, open ('hobby.csv', 'r', encoding='utf-8') as f2r:
+with open('users.csv', 'r', encoding='utf-8') as f1r, open('hobby.csv', 'r', encoding='utf-8') as f2r:
     for row in f1r:
-        list_users.append(row)
+        list_users.append(row.strip())
     for row in f2r:
-        list_hobby.append(row)
+        list_hobby.append(row.strip())
     if len(list_users) >= len(list_hobby):
         delta = len(list_users) - len(list_hobby)
         i = 0
         while i < delta:
-            list_hobby.append('None')
+            list_hobby.append(None)
             i += 1
         dict_user_hobby = dict(zip(list_users, list_hobby))
         with open('task_6_3_result.json', 'w', encoding='utf-8') as wjf:
@@ -51,4 +51,3 @@ with open('users.csv', 'r', encoding='utf-8') as f1r, open ('hobby.csv', 'r', en
         sys.exit(1)
 
 print('\n''end')
-
--- Danshin_Andrey_dz_6/task_6_4.py
+++ Danshin_Andrey_dz_6/task_6_4.py
@@ -3,40 +3,45 @@ import sys
 import json
 import string
 
-random_number = random.randrange(5, 10)
-random_number2 = random.randrange(1, 5)
+
+names = ['Александр', 'Андрей', 'Сергей', 'Михаил', 'Антон']
+second_names = ['Иванов', 'Смирнов', 'Кузнецов', 'Попов', 'Васильев']
+middle_names = ['Александрович', 'Андреевич', 'Сергеевич', 'Махайлович', 'Антонович']
+hobbies = ['бег', 'плавание', 'подводное плавание', 'велоспорт', 'танцы', 'чтение', 'пение', 'музыка']
+
 
 #генератор случайных ФИО
-def generate_random_id(length):
-    letters_upper = string.ascii_uppercase
-    letters_lower = string.ascii_lowercase
-    rand_string = ''.join(random.choice(letters_upper) for i in range(1)) + ''.join(random.choice(letters_lower) for i in range(length))
-    return f'{rand_string},{rand_string},{rand_string}'
+def generate_random_id():
+    random_name = random.choice(names)
+    random_second_names = random.choice(second_names)
+    random_middle_names = random.choice(middle_names)
+    return (f'{random_second_names},{random_name},{random_middle_names}')
 
-#генератор случайных хобби
-def generate_random_hobby(length):
-     letters_lower = string.ascii_lowercase
-     rand_string = ''.join(random.choice(letters_lower) for i in range(length))
-     random_hobby_list = (list(f'{rand_string},' for i in range(random_number2)))
-     return random_hobby_list
+
+# #генератор случайных хобби
+def generate_random_hobby():
+    random_hobby = random.choice(hobbies)
+    return random_hobby
+
 
 #создаем документы со именами и хобби
 with open('users.csv', 'w', encoding='utf-8') as fw:
-    for i in range(6):
-        fw.write(f'{generate_random_id(5)}\n')
-with open('hobby.csv', 'w', encoding='utf-8') as fw:
-    for i in range(5):
-        fw.write(f'{generate_random_hobby(10)}\n')
+     for i in range(6):
+         fw.write(f'{generate_random_id()}\n')
 
-#собираем словарь, проверяя разницу между длинами user  hobby
+with open('hobby.csv', 'w', encoding='utf-8') as fw:
+     for i in range(5):
+         fw.write(f'{generate_random_hobby()}, {generate_random_hobby()}\n')
+
+#собираем словарь, проверяя разницу между длинами user hobby
 list_users = []
 list_hobby = []
 
-with open('users.csv', 'r', encoding='utf-8') as f1r, open ('hobby.csv', 'r', encoding='utf-8') as f2r:
+with open('users.csv', 'r', encoding='utf-8') as f1r, open('hobby.csv', 'r', encoding='utf-8') as f2r:
     for row in f1r:
-        list_users.append(row)
+        list_users.append(row.strip())
     for row in f2r:
-        list_hobby.append(row)
+        list_hobby.append(row.strip())
     if len(list_users) >= len(list_hobby):
         delta = len(list_users) - len(list_hobby)
         i = 0
@@ -47,7 +52,5 @@ with open('users.csv', 'r', encoding='utf-8') as f1r, open ('hobby.csv', 'r', en
         with open('task_6_3_result.json', 'w', encoding='utf-8') as wjf:
             json.dump(dict_user_hobby, wjf, ensure_ascii=False, indent=2)
     else:
-        print(int(1)) #тут я не понял что значит выходим из скрипта с кодом 1
-
-print('\n''end')
-
+        sys.exit(1)
+    print('\n''end')
