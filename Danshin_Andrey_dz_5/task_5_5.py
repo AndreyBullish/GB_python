684bb47e2c3a1bd9a77ff7c8094ec41271070a8b
AndreyD <andreydv@bk.ru>
2022-02-17 10:59:07 +0300

    task_6

--- Danshin_Andrey_dz_5/task_5_5.py
+++ Danshin_Andrey_dz_5/task_5_5.py
@@ -1,11 +1,25 @@
-def get_uniq_numbers(src: list):
+src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+
+def get_uniq_numbers1(src: list):
     result = []
     for i in range(len(src)):
         if src.count(src[i]) == 1:
             result.append(src[i])
     return result
 
-src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-print(*get_uniq_numbers(src))
-#оптимизировать как не приходит в голову, т.к. в задании указано с сохр порядкового номера, то есть использовать множества нельзя получается. Посдкажите, плиз, как сделать оптимизацию
+print(*get_uniq_numbers1(src))
+
+def get_uniq_numbers2(src: list):
+    num_set = set()
+    num_set_final = set()
+    for num in src:
+        if not num in num_set:
+            num_set_final.add(num)
+        else:
+            num_set_final.discard(num)
+        num_set.add(num)
+print(get_uniq_numbers2(src))
+
+uniq_numbers = [num for num in src if src.count(num) == 1]
+print(uniq_numbers)
 print('\n' 'end')
