b7a4950ab6f0873812a3bde69c2b254fd4074c03
AndreyD <andreydv@bk.ru>
2022-02-22 15:45:37 +0300

    task_7

--- Danshin_Andrey_dz_7/task_7_1.py
+++ Danshin_Andrey_dz_7/task_7_1.py
@@ -6,9 +6,11 @@ inside_folders = ('settings', 'mainapp', 'adminapp', 'authapp')
 current_location = (os.path.dirname(os.path.abspath(__file__)))
 my_project_location = os.path.join(current_location, 'my_project')
 print(inside_folders)
-if not os.path.exists(my_project_location) or \
-        not os.path.exists(''.join([os.path.join(my_project_location, item) for item in inside_folders])):
-    for inside_folders in inside_folders:
-        os.makedirs(os.path.join(head_folder, inside_folders))
-
+try:
+    if not os.path.exists(my_project_location) or \
+            not os.path.exists(''.join([os.path.join(my_project_location, item) for item in inside_folders])):
+        for inside_folders in inside_folders:
+            os.makedirs(os.path.join(head_folder, inside_folders))
+except Exception as e:
+    print(f'{e.__class__.__name__} Поймали ошибку: {e}')
 print('end')
