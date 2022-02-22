7deadc56200b6621184b58ee3ab3eeed6992df9e
AndreyD <andreydv@bk.ru>
2022-02-22 13:50:09 +0300

    task_7

--- Danshin_Andrey_dz_7/task_7_1.py
+++ Danshin_Andrey_dz_7/task_7_1.py
@@ -1,13 +1,14 @@
 import os
 
 
-if __name__ == "__main__":
-    head_folder = ('my_project')
-    inside_folders = ('settings', 'mainapp', 'adminapp', 'authapp')
-    current_location = (os.path.dirname(os.path.abspath(__file__)))
-    dir_name = 'my_project'
-    if not os.path.exists(dir_name):
-        for dir1 in inside_folders:
-                os.makedirs(os.path.join(head_folder, dir1))
+head_folder = ('my_project')
+inside_folders = ('settings', 'mainapp', 'adminapp', 'authapp')
+current_location = (os.path.dirname(os.path.abspath(__file__)))
+my_project_location = os.path.join(current_location, 'my_project')
+print(inside_folders)
+if not os.path.exists(my_project_location) or \
+        not os.path.exists(''.join([os.path.join(my_project_location, item) for item in inside_folders])):
+    for inside_folders in inside_folders:
+        os.makedirs(os.path.join(head_folder, inside_folders))
 
 print('end')
