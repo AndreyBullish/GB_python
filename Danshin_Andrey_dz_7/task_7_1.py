bee9627ec22ed29f4d567ab8bd54e4c17c49a097
AndreyD <andreydv@bk.ru>
2022-02-23 16:57:32 +0300

    task_7

--- Danshin_Andrey_dz_7/task_7_1.py
+++ Danshin_Andrey_dz_7/task_7_1.py
@@ -1,16 +1,20 @@
 import os
+#ИЗМЕНИЛ НАЗВАНИЕ ПАПКИ MY_PROJECT ЧТОБЫ ВСЕ РАБОТАЛО И НЕ ПЕРЕСИКАЛОСЬ С TASK_3
 
+head_folder = ('my_project_first')
+list_inside_folders = ('settings', 'mainapp', 'adminapp', 'authapp')
 
-head_folder = ('my_project')
-inside_folders = ('settings', 'mainapp', 'adminapp', 'authapp')
-current_location = (os.path.dirname(os.path.abspath(__file__)))
-my_project_location = os.path.join(current_location, 'my_project')
-print(inside_folders)
-try:
-    if not os.path.exists(my_project_location) or \
-            not os.path.exists(''.join([os.path.join(my_project_location, item) for item in inside_folders])):
-        for inside_folders in inside_folders:
-            os.makedirs(os.path.join(head_folder, inside_folders))
-except Exception as e:
-    print(f'{e.__class__.__name__} Поймали ошибку: {e}')
+def func_creat_my_project(location):
+    current_location = location
+    my_project_location = os.path.join(current_location, head_folder)
+    try:
+        if not os.path.exists(my_project_location) or \
+                not os.path.exists(''.join([os.path.join(my_project_location, item) for item in list_inside_folders])):
+            for inside_folders in list_inside_folders:
+                os.makedirs(os.path.join(head_folder, inside_folders))
+    except Exception as e:
+        print(f'{e.__class__.__name__} Поймали ошибку: {e}')
+
+func_creat_my_project(__file__)
+
 print('end')
