1a802545a9d269e06e78c0eaa7800ba724a2a352
AndreyD <andreydv@bk.ru>
2022-03-15 18:05:02 +0300

    task_8

--- Danshin_Andrey_dz_9/task_1.py
+++ Danshin_Andrey_dz_9/task_1.py
@@ -9,10 +9,16 @@ class TrafficLight:
     }
 
     def running(self):
-        cycle = 10
-        for k, v in self.__color.items():
-            print(f'{k} {v} сек')
-            time.sleep(v)
+        cycle = 3
+        while cycle != 0: #сделал нескольков кругов
+            for k, v in self.__color.items():
+                if (list(self.__color.values())[0]) == 4 and (list(self.__color.values())[1]) == 2 and (list(self.__color.values())[2]) == 3:
+                    print(f'{k} {v} сек')
+                    time.sleep(v)
+
+                else:
+                    print('неверная последовательность светофора')
+            cycle -= 1
 
 if __name__ == '__main__':
     traffic = TrafficLight()
