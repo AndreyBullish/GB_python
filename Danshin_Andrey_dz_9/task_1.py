070994ddfedb8afa3dba0326c8c3846bb36da1b2
AndreyD <andreydv@bk.ru>
2022-03-15 18:05:26 +0300

    task_9

--- Danshin_Andrey_dz_9/task_1.py
+++ Danshin_Andrey_dz_9/task_1.py
@@ -15,7 +15,6 @@ class TrafficLight:
                 if (list(self.__color.values())[0]) == 4 and (list(self.__color.values())[1]) == 2 and (list(self.__color.values())[2]) == 3:
                     print(f'{k} {v} сек')
                     time.sleep(v)
-
                 else:
                     print('неверная последовательность светофора')
             cycle -= 1
